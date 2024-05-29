457052d43ed5fba7c08aa9842167fc8fb513e608
Hamza Mughal <hamza@prodesquare.com>
2024-05-29 07:18:38 +0500

    `app-container` setup

--- src/components/layouts/Root.tsx
+++ src/components/layouts/Root.tsx
@@ -1,16 +1,31 @@
 const Root = (): JSX.Element => (
-  <main>
-    <section>
-      <h1>Hello World!</h1>
+  <div id="app-container">
+    <header></header>
 
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero quaerat
-        enim molestias obcaecati similique, odit adipisci voluptates repudiandae
-        dolorem iusto repellat culpa earum labore. Animi, possimus explicabo.
-        Accusantium, fuga alias.
-      </p>
-    </section>
-  </main>
+    <main>
+      <section>
+        <h1>Hello World!</h1>
+
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero
+          quaerat enim molestias obcaecati similique, odit adipisci voluptates
+          repudiandae dolorem iusto repellat culpa earum labore. Animi, possimus
+          explicabo. Accusantium, fuga alias.
+        </p>
+      </section>
+
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+        laudantium aliquid necessitatibus libero fugiat enim quibusdam
+        exercitationem dicta maiores ipsam? In, nihil veritatis! Unde, nihil
+        porro libero voluptas repellendus accusamus.
+      </div>
+    </main>
+
+    <footer>
+      Generated with <a href="#">Domain Seller</a>
+    </footer>
+  </div>
 );
 
 export default Root;
