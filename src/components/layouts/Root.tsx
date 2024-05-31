6f592caf3856a0913ca1727488fe6f3eb28e9df0
Hamza Mughal <hamza@prodesquare.com>
2024-06-01 04:53:27 +0500

    site footer and logo

--- src/components/layouts/Root.tsx
+++ src/components/layouts/Root.tsx
@@ -1,20 +1,34 @@
+import Footer from "@/components/sections/Footer";
+
 const Root = (): JSX.Element => (
   <div id="app-container">
     <main>
-      <section className="app-title-container">
-        <div>
-          <h1>Domain For Sale!</h1>
+      <section className="app-title-wrapper">
+        <div className="app-title-container">
+          <div>
+            <a href="https://prodesquare.com">
+              <img src="/prodesquare-logo.svg" alt="Site Logo" />
+            </a>
+          </div>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
-            tenetur ex magni et libero reprehenderit fuga omnis deserunt nobis
-            inventore neque consectetur molestiae dolores dolorem rerum earum
-            illo, quidem ullam.
-          </p>
+          <div className="app-title">
+            <h1>Domain For Sale!</h1>
 
-          <button onClick={() => window.open("mailto:hamza@prodesquare.com")}>
-            Contact Me
-          </button>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+              tenetur ex magni et libero reprehenderit fuga omnis deserunt nobis
+              inventore neque consectetur molestiae dolores dolorem rerum earum
+              illo, quidem ullam.
+            </p>
+
+            <button onClick={() => window.open("mailto:hamza@prodesquare.com")}>
+              Contact Me
+            </button>
+          </div>
+
+          <div className="footer-container">
+            <Footer />
+          </div>
         </div>
       </section>
 
@@ -49,6 +63,10 @@ const Root = (): JSX.Element => (
 
             <button type="submit">Submit</button>
           </form>
+        </div>
+
+        <div className="mobile-footer-container">
+          <Footer />
         </div>
       </section>
     </main>
---
+++ src/components/sections/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = (): JSX.Element => (
+  <footer>
+    &copy; {new Date().getFullYear()}{" "}
+    <a href="https://prodesquare.com">ProDeSquare</a> / All rights reserved -
+    Static webpage generated with <a href="#">Domain Seller</a>
+  </footer>
+);
+
+export default Footer;
