ebf22a76b2d1e9d4ed796e43010f950a1007de62
Hamza Mughal <hamza@prodesquare.com>
2024-06-03 03:09:40 +0500

    checking if the name is valid and email is valid and not from same domain

--- src/helpers/validate.ts
+++ src/helpers/validate.ts
@@ -1,3 +1,5 @@
+import app from "@/data/app";
+
 const requestHasEmptyFields = (req: Record<string, string>): boolean => {
   for (const key in req) {
     if (req[key] === "") {
@@ -8,15 +10,28 @@ const requestHasEmptyFields = (req: Record<string, string>): boolean => {
   return false;
 };
 
+const requestHasInvalidData = (req: Record<string, string>): boolean => {
+  const emailRegex: RegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  if (req.name.length < 3 || !emailRegex.test(req.email)) return true;
+
+  return false;
+};
+
 const alreadyRequested = (email: string): boolean => {
   return (
     JSON.parse(localStorage.getItem("history") || "[]") as Array<string>
   ).includes(email);
 };
 
+const emailFromSameDomain = (email: string): boolean => {
+  return email.split("@")[1] === app.domain;
+};
+
 const validationCheck = (req: Record<string, string>): boolean => {
   if (requestHasEmptyFields(req)) return false;
   if (alreadyRequested(req.email)) return false;
+  if (emailFromSameDomain(req.email)) return false;
+  if (requestHasInvalidData(req)) return false;
 
   return true;
 };
